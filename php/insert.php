6935782793232e4da638626d6075d81f6b85d65a
Pedro Gil <pedroaggil@gmail.com>
2022-04-26 22:25:16 -0300

    Update insert.php

--- php/insert.php
+++ php/insert.php
@@ -2,15 +2,18 @@
 
 	$nome = $_POST['name'];
 	$desc = $_POST['descricao'];
-	$urgencia = $_POST['urg'];
-	$importancia = $_POST['imp'];
+	$urgencia = $_POST['urg']; //
+	$importancia = $_POST['imp']; //
 	$prazo = $_POST['prazo'];
-	$setor = $_POST['sector'];
-	$requerente = $_POST['requerente'];
+	$setor = $_POST['sector']; //
+	$requerente = $_POST['requerente']; // Esse aqui não vai inserir, vai puxar pra inteligência
 
 	$insert_urg = "INSERT INTO tb_urgencia (cd_urg, ds_urg) VALUES (null, '". $urgencia ."')";
 	$insert_imp = "INSERT INTO tb_urgencia (cd_imp, ds_imp) VALUES (null, '". $importancia ."')";
 
-	$insert_setor = "INSERT INTO tb_setor (cd_setor, nm_setor) VALUES (null, '". $setor ."')"; 
+	$insert_setor = "INSERT INTO tb_setor (cd_setor, nm_setor) VALUES (null, '". $setor ."')";
+
+	$insert_requisicao = "INSERT INTO tb_requisicao (cd_requisicao, nm_req, ds_descricao, dt_data)
+	VALUES (null, '". $nome ."', '". $desc ."', '". $prazo ."')";
 
  ?>
